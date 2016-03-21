bdbb1cd706270a1d3fc23f30bdc62ba4371f7af4
Brett <brettrheenan@gmail.com>
2016-03-21 23:15:35 +0100

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -13,42 +13,65 @@ $(document).ready(function(){
   var theArtist = artists[Math.floor(Math.random()) * artists.length];
   var songToPlay = jukeboxSongs[theArtist];
 
-  function randomNumber() {
-    return Math.floor(Math.random());
-  }
+  // function randomNumber() {
+  //   return Math.floor(Math.random());
+  // }
 
   function menu(){
-    $('#menu').on('click', menuOptions);
+    $('#menu').on('click', function() {
+      $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+    });
   }
 
-   function menuOptions() {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  }
+  //  function menuOptions() {
+  //   $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+  // }
 
-  function nowPlaying(){
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  }
+  // function nowPlaying(){
+  //   $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  //   return "now playing " + songToPlay + " by " + theArtist;
+  // }
 
   function play(){
-    $('#play').on('click', nowPlaying)
+    $('#play').on('click', function() {
+      $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+      return "now playing " + songToPlay + " by " + theArtist;
+    });
   }
 
-  function nowPausing(){
-    $('#songPaused').html(songToPlay + " is paused");
-  }
+  // function nowPausing(){
+  //   $('#songPaused').html(songToPlay + " is paused");
+  // }
 
   function pause(){
-    $('#pause').on('click', nowPausing);
+    $('#pause').on('click', function() {
+      $('#songPaused').html(songToPlay + " is paused");
+    });
   }
-   function showSongs(){
-    var songs = "";
-    for (var key in jukeboxSongs) {
-    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-  }
-    $('#showSongs').html(songs);
-  }
+   
+  // function showSongs(){
+  //   var songs = "";
+  //   for (var key in jukeboxSongs) {
+  //     songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  //   }
+  //   $('#showSongs').html(songs);
+  // }
+
   function show(){
-    $('#show').on('click', showSongs);
+    $('#show').on('click', function() {
+      var songs = "";
+      for (var key in jukeboxSongs) {
+        songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+      };
+      $('#showSongs').html(songs);
+    });
   }
+
+
+
+
+
+
+
+
 
